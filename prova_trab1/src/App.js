f74caae93b82612d1812866a6fa96ef70b199bbf
EmerDumke <emersondumke@hotmail.com>
2022-11-03 20:00:31 -0300

    atividade 1 cod

--- prova_trab1/src/App.js
+++ prova_trab1/src/App.js
@@ -1,23 +1,161 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  <div class='app'>  
+    <div class="title-container">
+        <h2 class="text-exp">Projeto de Programação Web II</h2>
+    </div>
+    <section class="card-container">
+    <div class="container">
+        <div class="user-image">
+            <img src="https://img.freepik.com/premium-vector/ninja-samurai-warrior-drawing-art-sketch_655523-19.jpg?w=2000"
+                alt="ninja_user"/>
+        </div>
+        <div class="content">
+            <h3 class="name">Usuário de Trabalho</h3>
+            <p class="username">@usuario_TI</p>
+
+            <div class="links">
+                <a class="facebook" href="https://www.facebook.com/" target="_blank" title="GFG_facebook">
+                    <i class="fab fa-facebook"></i>
+                </a>
+                <a class="git" href="https://github.com/Ferreira2912" title="TIuser_github" target="_blank">
+                    <i class="fab fa-github-square"></i>
+                </a>
+                <a class="linkedin" href="https://www.geeksforgeeks.org/tag/linkedin/" title="GFG_linkedin" target="_blank"/>
+                    <i class="fab fa-linkedin"></i>
+                <a class="insta" href="https://www.instagram.com/" target="_blank" title="GFG_instagram">
+                    <i class="fab fa-instagram-square"></i>
+                </a>
+            </div>
+            
+                <p class="details"> Um teste para o trabalho de TI</p>
+
+        </div>
+    </div>
+    <div class="container">
+        <div class="user-image">
+            <img src="https://img.freepik.com/premium-vector/ninja-samurai-warrior-drawing-art-sketch_655523-19.jpg?w=2000"
+                alt="ninja_user"/>
+        </div>
+        <div class="content">
+            <h3 class="name">Usuário de Trabalho</h3>
+            <p class="username">@usuario_TI</p>
+
+            <div class="links">
+                <a class="facebook" href="https://www.facebook.com/" target="_blank" title="GFG_facebook">
+                    <i class="fab fa-facebook"></i>
+                </a>
+                <a class="git" href="https://github.com/Ferreira2912" title="TIuser_github" target="_blank">
+                    <i class="fab fa-github-square"></i>
+                </a>
+                <a class="linkedin" href="https://www.geeksforgeeks.org/tag/linkedin/" title="GFG_linkedin" target="_blank"/>
+                    <i class="fab fa-linkedin"></i>
+                <a class="insta" href="https://www.instagram.com/" target="_blank" title="GFG_instagram">
+                    <i class="fab fa-instagram-square"></i>
+                </a>
+            </div>
+            
+                <p class="details"> Um teste para o trabalho de TI</p>
+
+        </div>
+    </div>
+    <div class="container">
+        <div class="user-image">
+            <img src="https://img.freepik.com/premium-vector/ninja-samurai-warrior-drawing-art-sketch_655523-19.jpg?w=2000"
+                alt="ninja_user"/>
+        </div>
+        <div class="content">
+            <h3 class="name">Usuário de Trabalho</h3>
+            <p class="username">@usuario_TI</p>
+
+            <div class="links">
+                <a class="facebook" href="https://www.facebook.com/" target="_blank" title="GFG_facebook">
+                    <i class="fab fa-facebook"></i>
+                </a>
+                <a class="git" href="https://github.com/Ferreira2912" title="TIuser_github" target="_blank">
+                    <i class="fab fa-github-square"></i>
+                </a>
+                <a class="linkedin" href="https://www.geeksforgeeks.org/tag/linkedin/" title="GFG_linkedin" target="_blank"/>
+                    <i class="fab fa-linkedin"></i>
+                <a class="insta" href="https://www.instagram.com/" target="_blank" title="GFG_instagram">
+                    <i class="fab fa-instagram-square"></i>
+                </a>
+            </div>
+            
+                <p class="details"> Um teste para o trabalho de TI</p>
+
+        </div>
+    </div>
+    <div class="container">
+        <div class="user-image">
+            <img src="https://img.freepik.com/premium-vector/ninja-samurai-warrior-drawing-art-sketch_655523-19.jpg?w=2000"
+                alt="ninja_user"/>
+        </div>
+        <div class="content">
+            <h3 class="name">Usuário de Trabalho</h3>
+            <p class="username">@usuario_TI</p>
+
+            <div class="links">
+                <a class="facebook" href="https://www.facebook.com/" target="_blank" title="GFG_facebook">
+                    <i class="fab fa-facebook"></i>
+                </a>
+                <a class="git" href="https://github.com/Ferreira2912" title="TIuser_github" target="_blank">
+                    <i class="fab fa-github-square"></i>
+                </a>
+                <a class="linkedin" href="https://www.geeksforgeeks.org/tag/linkedin/" title="GFG_linkedin" target="_blank"/>
+                    <i class="fab fa-linkedin"></i>
+                <a class="insta" href="https://www.instagram.com/" target="_blank" title="GFG_instagram">
+                    <i class="fab fa-instagram-square"></i>
+                </a>
+            </div>
+            
+                <p class="details"> Um teste para o trabalho de TI</p>
+
+        </div>
+    </div>
+    <div class="container">
+        <div class="user-image">
+            <img src="https://img.freepik.com/premium-vector/ninja-samurai-warrior-drawing-art-sketch_655523-19.jpg?w=2000"
+                alt="ninja_user"/>
+        </div>
+        <div class="content">
+            <h3 class="name">Usuário de Trabalho</h3>
+            <p class="username">@usuario_TI</p>
+
+            <div class="links">
+                <a class="facebook" href="https://www.facebook.com/" target="_blank" title="GFG_facebook">
+                    <i class="fab fa-facebook"></i>
+                </a>
+                <a class="git" href="https://github.com/Ferreira2912" title="TIuser_github" target="_blank">
+                    <i class="fab fa-github-square"></i>
+                </a>
+                <a class="linkedin" href="https://www.geeksforgeeks.org/tag/linkedin/" title="GFG_linkedin" target="_blank"/>
+                    <i class="fab fa-linkedin"></i>
+                <a class="insta" href="https://www.instagram.com/" target="_blank" title="GFG_instagram">
+                    <i class="fab fa-instagram-square"></i>
+                </a>
+            </div>
+            
+                <p class="details"> Um teste para o trabalho de TI</p>
+
+        </div>
+    </div>
+</section>
+    <div class="text-container">
+        <h3 class="text-exp">Crie um programa para ajudar o amigo marombeiro, 
+            deve ter uma tela para cadastrar o exercício 
+            (nome do exercício, quantidade de series e repetições) e uma opção 
+            para marcar qual ele já concluiu.</h3>
+    </div>
+
+    <script src="https://kit.fontawesome.com/704ff50790.js"
+        crossorigin="anonymous">
+    </script>
+    
     </div>
   );
 }

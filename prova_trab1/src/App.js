aa003fb79836d24df0dfdea3cdc452a5d047fbb6
EmerDumke <emersondumke@hotmail.com>
2022-11-03 20:40:48 -0300

    atividade 1 class

--- prova_trab1/src/App.js
+++ prova_trab1/src/App.js
@@ -1,142 +1,142 @@
-
+import logo from './joao.jpeg'
 import './App.css';
 
 function App() {
   return (
 
-  <div class='app'>  
-    <div class="title-container">
-        <h2 class="text-exp">Projeto de Programação Web II</h2>
+  <div className='app'>  
+    <div className="title-container">
+        <h2 className="text-exp">Projeto de Programação Web II</h2>
     </div>
-    <section class="card-container">
-    <div class="container">
-        <div class="user-image">
-            <img src="https://img.freepik.com/premium-vector/ninja-samurai-warrior-drawing-art-sketch_655523-19.jpg?w=2000"
+    <section classNameName="card-container">
+    <div className="container">
+        <div className="user-image">
+            <img src={logo}
                 alt="ninja_user"/>
         </div>
-        <div class="content">
-            <h3 class="name">João Pedro</h3>
-            <p class="username">@jpferreira</p>
+        <div className="content">
+            <h3 className="name">João Pedro</h3>
+            <p className="username">@jpferreira</p>
 
-            <div class="links">
-                <a class="facebook" href="https://www.facebook.com/" target="_blank" title="GFG_facebook">
-                    <i class="fab fa-facebook"></i>
+            <div className="links">
+                <a className="facebook" href="https://www.facebook.com/joaopedro.marques.792197" target="_blank" title="GFG_facebook">
+                    <i className="fab fa-facebook"></i>
                 </a>
-                <a class="git" href="https://github.com/Ferreira2912" title="TIuser_github" target="_blank">
-                    <i class="fab fa-github-square"></i>
+                <a className="git" href="https://github.com/Ferreira2912" title="TIuser_github" target="_blank">
+                    <i className="fab fa-github-square"></i>
                 </a>
-                <a class="insta" href="https://www.instagram.com/joao_pedromf29" target="_blank" title="GFG_instagram">
-                    <i class="fab fa-instagram-square"></i>
+                <a className="insta" href="https://www.instagram.com/joao_pedromf29" target="_blank" title="GFG_instagram">
+                    <i className="fab fa-instagram-square"></i>
                 </a>
             </div>
             
-                <p class="details"> Um teste para o trabalho de TI</p>
+                <p className="details"> Um teste para o trabalho de TI</p>
 
         </div>
     </div>
-    <div class="container">
-        <div class="user-image">
+    <div className="container">
+        <div className="user-image">
             <img src="https://img.freepik.com/premium-vector/ninja-samurai-warrior-drawing-art-sketch_655523-19.jpg?w=2000"
                 alt="ninja_user"/>
         </div>
-        <div class="content">
-            <h3 class="name">Usuário de Trabalho</h3>
-            <p class="username">@usuario_TI</p>
+        <div className="content">
+            <h3 className="name">Usuário de Trabalho</h3>
+            <p className="username">@usuario_TI</p>
 
-            <div class="links">
-                <a class="facebook" href="https://www.facebook.com/" target="_blank" title="GFG_facebook">
-                    <i class="fab fa-facebook"></i>
+            <div className="links">
+                <a className="facebook" href="https://www.facebook.com/" target="_blank" title="GFG_facebook">
+                    <i className="fab fa-facebook"></i>
                 </a>
-                <a class="git" href="https://github.com/Ferreira2912" title="TIuser_github" target="_blank">
-                    <i class="fab fa-github-square"></i>
+                <a className="git" href="https://github.com/Ferreira2912" title="TIuser_github" target="_blank">
+                    <i className="fab fa-github-square"></i>
                 </a>
-                <a class="insta" href="https://www.instagram.com/" target="_blank" title="GFG_instagram">
-                    <i class="fab fa-instagram-square"></i>
+                <a className="insta" href="https://www.instagram.com/" target="_blank" title="GFG_instagram">
+                    <i className="fab fa-instagram-square"></i>
                 </a>
             </div>
             
-                <p class="details"> Um teste para o trabalho de TI</p>
+                <p className="details"> Um teste para o trabalho de TI</p>
 
         </div>
     </div>
-    <div class="container">
-        <div class="user-image">
+    <div className="container">
+        <div className="user-image">
             <img src="https://img.freepik.com/premium-vector/ninja-samurai-warrior-drawing-art-sketch_655523-19.jpg?w=2000"
                 alt="ninja_user"/>
         </div>
-        <div class="content">
-            <h3 class="name">Amilto Junior Smaniotto</h3>
-            <p class="username">@Amilto.J</p>
+        <div className="content">
+            <h3 className="name">Amilto Junior Smaniotto</h3>
+            <p className="username">@Amilto.J</p>
 
-            <div class="links">
-                <a class="facebook" href="https://www.facebook.com/amiltojunior.smaniotto.3" target="_blank" title="GFG_facebook">
-                    <i class="fab fa-facebook"></i>
+            <div className="links">
+                <a className="facebook" href="https://www.facebook.com/amiltojunior.smaniotto.3" target="_blank" title="GFG_facebook">
+                    <i className="fab fa-facebook"></i>
                 </a>
-                <a class="git" href="https://github.com/AmiltoJ" title="TIuser_github" target="_blank">
-                    <i class="fab fa-github-square"></i>
+                <a className="git" href="https://github.com/AmiltoJ" title="TIuser_github" target="_blank">
+                    <i className="fab fa-github-square"></i>
                 </a>
-                <a class="insta" href="https://www.instagram.com/amilto._.exe" target="_blank" title="GFG_instagram">
-                    <i class="fab fa-instagram-square"></i>
+                <a className="insta" href="https://www.instagram.com/amilto._.exe" target="_blank" title="GFG_instagram">
+                    <i className="fab fa-instagram-square"></i>
                 </a>
             </div>
             
-                <p class="details"> Um teste para o trabalho de TI</p>
+                <p className="details"> Um teste para o trabalho de TI</p>
 
         </div>
     </div>
-    <div class="container">
-        <div class="user-image">
+    <div className="container">
+        <div className="user-image">
             <img src="https://img.freepik.com/premium-vector/ninja-samurai-warrior-drawing-art-sketch_655523-19.jpg?w=2000"
                 alt="ninja_user"/>
         </div>
-        <div class="content">
-            <h3 class="name">Usuário de Trabalho</h3>
-            <p class="username">@usuario_TI</p>
+        <div className="content">
+            <h3 className="name">Usuário de Trabalho</h3>
+            <p className="username">@usuario_TI</p>
 
-            <div class="links">
-                <a class="facebook" href="https://www.facebook.com/" target="_blank" title="GFG_facebook">
-                    <i class="fab fa-facebook"></i>
+            <div className="links">
+                <a className="facebook" href="https://www.facebook.com/" target="_blank" title="GFG_facebook">
+                    <i className="fab fa-facebook"></i>
                 </a>
-                <a class="git" href="https://github.com/Ferreira2912" title="TIuser_github" target="_blank">
-                    <i class="fab fa-github-square"></i>
+                <a className="git" href="https://github.com/Ferreira2912" title="TIuser_github" target="_blank">
+                    <i className="fab fa-github-square"></i>
                 </a>
-                <a class="insta" href="https://www.instagram.com/" target="_blank" title="GFG_instagram">
-                    <i class="fab fa-instagram-square"></i>
+                <a className="insta" href="https://www.instagram.com/" target="_blank" title="GFG_instagram">
+                    <i className="fab fa-instagram-square"></i>
                 </a>
             </div>
             
-                <p class="details"> Um teste para o trabalho de TI</p>
+                <p className="details"> Um teste para o trabalho de TI</p>
 
         </div>
     </div>
-    <div class="container">
-        <div class="user-image">
+    <div className="container">
+        <div className="user-image">
             <img src="https://img.freepik.com/premium-vector/ninja-samurai-warrior-drawing-art-sketch_655523-19.jpg?w=2000"
                 alt="ninja_user"/>
         </div>
-        <div class="content">
-            <h3 class="name">Usuário de Trabalho</h3>
-            <p class="username">@usuario_TI</p>
+        <div className="content">
+            <h3 className="name">Usuário de Trabalho</h3>
+            <p className="username">@usuario_TI</p>
 
-            <div class="links">
-                <a class="facebook" href="https://www.facebook.com/" target="_blank" title="GFG_facebook">
-                    <i class="fab fa-facebook"></i>
+            <div className="links">
+                <a className="facebook" href="https://www.facebook.com/" target="_blank" title="GFG_facebook">
+                    <i className="fab fa-facebook"></i>
                 </a>
-                <a class="git" href="https://github.com/Ferreira2912" title="TIuser_github" target="_blank">
-                    <i class="fab fa-github-square"></i>
+                <a className="git" href="https://github.com/Ferreira2912" title="TIuser_github" target="_blank">
+                    <i className="fab fa-github-square"></i>
                 </a>
-                <a class="insta" href="https://www.instagram.com/" target="_blank" title="GFG_instagram">
-                    <i class="fab fa-instagram-square"></i>
+                <a className="insta" href="https://www.instagram.com/" target="_blank" title="GFG_instagram">
+                    <i className="fab fa-instagram-square"></i>
                 </a>
             </div>
             
-                <p class="details"> Um teste para o trabalho de TI</p>
+                <p className="details"> Um teste para o trabalho de TI</p>
 
         </div>
     </div>
 </section>
-    <div class="text-container">
-        <h3 class="text-exp">Crie um programa para ajudar o amigo marombeiro, 
+    <div className="text-container">
+        <h3 className="text-exp">Crie um programa para ajudar o amigo marombeiro, 
             deve ter uma tela para cadastrar o exercício 
             (nome do exercício, quantidade de series e repetições) e uma opção 
             para marcar qual ele já concluiu.</h3>

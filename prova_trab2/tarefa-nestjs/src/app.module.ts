9943179a0430a9517870944b0a6af598cd428b35
WilliamSchreiner <williamg.schreiner@gmail.com>
2022-11-29 22:05:18 -0300

    controller

--- prova_trab2/tarefa-nestjs/src/app.module.ts
+++ prova_trab2/tarefa-nestjs/src/app.module.ts
@@ -1,27 +1,29 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm'; 
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { tarefaController } from './tarefa.controller';
-
 import { Tarefa } from './tarefa.entity';
+import { TarefaService } from './tarefa.service';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: 'postgres',
       host: 'localhost',
       port: 5432,
-      username: 'root',
+      username: 'postgres',
       password: 'senha',
       database: 'tarefa',
-      entities: [Tarefa], // entidades/classes],
-      synchronize: false
+      entities: [
+        Tarefa // entidades/classes
+      ],
+      synchronize: true
     }),
     TypeOrmModule.forFeature([Tarefa])
   ],
   controllers: [AppController, tarefaController],
-  providers: [AppService],
+  providers: [AppService, TarefaService],
 })
 export class AppModule {}
